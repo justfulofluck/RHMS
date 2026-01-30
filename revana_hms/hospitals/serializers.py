724d20a82e5d60ab148113c4b2cdcaf9c9b4f757
Bhavan <bhavanbadhe@outlook.com>
2026-01-30 15:50:32 +0530

    Bhavan - API changes
    Fields Added:
    
    email
    p_number (Mapped from phone_number)
    address
    hospital_type
    total_doctors (Count of doctors)
    total_departments (Count of departments)

--- revana_hms/hospitals/serializers.py
+++ revana_hms/hospitals/serializers.py
@@ -16,9 +16,19 @@ class HospitalRegisterSerializer(serializers.ModelSerializer):
 
 
 class HospitalPublicSerializer(serializers.ModelSerializer):
+    p_number = serializers.CharField(source='phone_number')
+    total_doctors = serializers.SerializerMethodField()
+    total_departments = serializers.SerializerMethodField()
+
     class Meta:
         model = Hospital
-        fields = ["id", "name", "city", "logo"]
+        fields = ["id", "name", "city", "logo", "hospital_type", "email", "p_number", "address", "total_doctors", "total_departments"]
+
+    def get_total_doctors(self, obj):
+        return obj.doctor_set.count()
+
+    def get_total_departments(self, obj):
+        return obj.departments.count()
 
 class DepartmentSerializer(serializers.ModelSerializer):
     class Meta:
