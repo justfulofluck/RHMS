b3b695ce3769e8a8d22ed5f3320f97998d8ae1b2
Bhavan Badhe <bhavanbadhe@outlook.com>
2026-02-02 11:53:09 +0000

    hai bhagwan smbhal lena

--- revana_hms/hospitals/models.py
+++ revana_hms/hospitals/models.py
@@ -60,7 +60,7 @@ class HospitalAdmin(models.Model):
 class Department(models.Model):
     hospital = models.ForeignKey('hospitals.Hospital', on_delete=models.CASCADE, related_name="departments")
     name = models.CharField(max_length=100)
-    code = models.CharField(max_length=10, default='GEN')
+    #code = models.CharField(max_length=10, default='GEN')
 
     def __str__(self):
         return f"{self.name} ({self.hospital.name})"
